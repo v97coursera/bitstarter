f7c346fdb8e33c456cc974ae5845fec8ba1a3be6
Phil <voltage97+coursera@gmail.com>
2013-07-08 15:00:33 -0700

    Modified web.js to read file and display contents

--- web.js
+++ web.js
@@ -2,9 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+fs.readFileSync('/index.html', 'utf-8', function(err, data){
+    if(err)throw err;
+       
+    //var buf = new Buffer(data, 0,data.length);
+
+    app.get('/', function(request, response) {
+      response.send(data);
+    });
+}
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
