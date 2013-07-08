0d7ddb53eb665c20e19ba9d12c19077b34e2f4a1
Phil <voltage97+coursera@gmail.com>
2013-07-08 15:23:57 -0700

    File reading working

--- web.js
+++ web.js
@@ -1,18 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('/index.html', 'utf-8', function(err, data){
-    if(err)throw err;
-       
-    //var buf = new Buffer(data, 0,data.length);
+var message = fs.readFileSync('./index.html', 'utf-8');
 
-    app.get('/', function(request, response) {
-      response.send(data);
-    });
-}
+app.get('/', function(request, response) {
+	    response.send(mesage);
+	});
 
 
+console.log(message);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
