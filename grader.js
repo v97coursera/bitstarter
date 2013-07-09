9400f214663ae34298ae4cae792aed5dba1b69a3
Phil <voltage97+coursera@gmail.com>
2013-07-09 15:15:22 -0700

    Grader working, needs cleanup

--- grader.js
+++ grader.js
@@ -24,17 +24,20 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLINDEX_DEFAULT = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_enode xit_code
     }
     return instr;
 };
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -44,16 +47,52 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+var checkHtmlFile = function(htmlfile, checksfile, urlindex) {
+    
+    //if we're passed a URL, check if it exists then parse it with cheerio
+    if(urlindex && urlindex.length > 0){
+	console.log("using urlindex");
+
+	var urlString = urlindex.toString();
+    
+	restler.get(urlString).on('complete', function(result){
+	if(result instanceof Error){
+	    console.log("%s Url does not exist. Exiting.", urlString);
+	    process.exit(1);
+	}
+	else
+	{
+	    console.log("%s Url exists!", urlString);
+	    $ = cheerio.load(result.toString());	    
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;	
+		out[checks[ii]] = present;
+	    }
+	    console.log(out);
+	    return out;
+	}
+    });
+
+	
     }
-    return out;
+    else
+    {
+	console.log("htmlfile exists")
+	$ = cheerioHtmlFile(htmlfile);	  
+
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;	
+            out[checks[ii]] = present;
+	}
+	return out;
+    }        
+   
 };
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,8 +104,10 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <file_url>', 'Url of index.html', URLINDEX_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson = checkHtmlFile(program.file, program.checks, program.url);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
